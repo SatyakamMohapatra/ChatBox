ae263232c9d3de2c709cf862a06bfd10bc734580
satya123zx <satyakam.mohapatar91@gmail.com>
2016-09-20 01:11:19 +0530

    Git Ignore Added

--- ChartBoxApp/src/main/java/com/chartboxapp/controller/LoginController.java
+++ ChartBoxApp/src/main/java/com/chartboxapp/controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.chartboxapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class LoginController {
 	
 	@RequestMapping(path="/hello",method = RequestMethod.GET)
