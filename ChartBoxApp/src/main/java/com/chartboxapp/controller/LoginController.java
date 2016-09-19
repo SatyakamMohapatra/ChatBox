a32f2efd5fe7718a4e81dde17bcadf14085ea576
satyakam.m <satyakam.mohapatra91@gmailcom>
2016-09-19 21:21:17 +0530

    New Controler

--- ChartBoxApp/src/main/java/com/chartboxapp/controller/LoginController.java
+++ ChartBoxApp/src/main/java/com/chartboxapp/controller/LoginController.java
@@ -1,5 +1,15 @@
 package com.chartboxapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 public class LoginController {
+	
+	@RequestMapping(path="/hello",method = RequestMethod.GET)
+	public ModelAndView loginRedirect() {
+		System.out.println("first print");
+		return new ModelAndView("hello");
+	}
 
 }
