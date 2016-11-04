39f10ef29251fcdcfa4c30a647177f7597d7dded
satya123zx <satyakam.mohapatar91@gmail.com>
2016-11-04 12:32:02 +0530

    --Exception handling

---
+++ ChartBoxApp/src/main/java/com/chartboxapp/controller/RestApi.java
@@ -0,0 +1,44 @@
+package com.chartboxapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chartboxapp.dto.RegisterDto;
+import com.chartboxapp.exception.UserNotFoundException;
+
+@RestController
+public class RestApi {
+	
+	@RequestMapping(produces="application/json",method=RequestMethod.GET,path="/api/getuserapi")
+	public RegisterDto getUserApi(@RequestParam("userName") String username,
+								  @RequestParam("userID") int userID){
+		
+		RegisterDto dto = new RegisterDto();
+		dto.setUserID(userID);
+		dto.setUserEmail("satya@gmail.com");
+		dto.setUserName(username);
+		dto.setUserPassword("satya");
+		
+		
+		return dto;
+	}
+	@RequestMapping(method=RequestMethod.PUT,path="/api/setuserapi")
+	public ResponseEntity<RegisterDto> setUserApi(@RequestBody RegisterDto dto){
+		
+		
+		if(dto.getUserID()==10){
+			System.out.println(dto);
+			return new ResponseEntity<RegisterDto>(dto,HttpStatus.OK);
+		}else{
+			throw new UserNotFoundException(dto.getUserID());
+		}
+	
+		
+	}
+
+}
---
+++ ChartBoxApp/src/main/java/com/chartboxapp/exception/UserNotFoundException.java
@@ -0,0 +1,20 @@
+
+package com.chartboxapp.exception;
+
+public class UserNotFoundException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int userID ;
+	
+	public UserNotFoundException(int userID) {
+		this.userID=userID;
+	}
+	@Override
+	public String toString() {
+		return "userID "+userID+" doesnot exist";
+	}
+
+}
