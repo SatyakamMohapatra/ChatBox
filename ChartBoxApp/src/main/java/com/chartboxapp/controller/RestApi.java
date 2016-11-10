b6f4a67a48dae002e8b76764246a09f794a9614d
satyakam.m <satyakam.mohapatra91@gmailcom>
2016-11-10 21:37:29 +0530

    created consum service

--- ChartBoxApp/src/main/java/com/chartboxapp/controller/RestApi.java
+++ ChartBoxApp/src/main/java/com/chartboxapp/controller/RestApi.java
@@ -2,6 +2,7 @@ package com.chartboxapp.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,31 +15,33 @@ import com.chartboxapp.exception.UserNotFoundException;
 @RestController
 public class RestApi {
 	
-	@RequestMapping(produces="application/json",method=RequestMethod.GET,path="/api/getuserapi")
-	public RegisterDto getUserApi(@RequestParam("userName") String username,
-								  @RequestParam("userID") int userID){
+	@RequestMapping(produces="application/json",method=RequestMethod.GET,path="/api/getuserapi/{id}")
+	public RegisterDto getUserApi(@PathVariable("id") Integer userID){
+		RegisterDto dto = new RegisterDto();
+		dto.setUserID(userID);
+		dto.setUserEmail("satya@gmail.com");
+		dto.setUserName("satya");
+		dto.setUserPassword("satya");
+		return dto;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,path="/api/setuserapi")
+	public ResponseEntity<RegisterDto> setUserApi(@RequestBody Integer userID){	
 		
 		RegisterDto dto = new RegisterDto();
 		dto.setUserID(userID);
 		dto.setUserEmail("satya@gmail.com");
-		dto.setUserName(username);
+		dto.setUserName("satya");
 		dto.setUserPassword("satya");
+		return new ResponseEntity<RegisterDto>(dto,HttpStatus.OK);
 		
-		
-		return dto;
-	}
-	@RequestMapping(method=RequestMethod.PUT,path="/api/setuserapi")
-	public ResponseEntity<RegisterDto> setUserApi(@RequestBody RegisterDto dto){
-		
-		
-		if(dto.getUserID()==10){
+		/*if(dto.getUserID()==10){
 			System.out.println(dto);
 			return new ResponseEntity<RegisterDto>(dto,HttpStatus.OK);
 		}else{
-			throw new UserNotFoundException(dto.getUserID());
+			throw new UserNotFoundException(dto.getUserID());*/
 		}
 	
 		
 	}
 
-}
