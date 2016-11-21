b7662b0117a6923397de9a438aa554d703509359
satya123zx <satyakam.mohapatar91@gmail.com>
2016-11-21 11:32:18 +0530

    ==security removed

--- ChartBoxApp/src/main/java/com/chartboxapp/security/ChartBoxWebSecurityInitilazer.java
+++ ChartBoxApp/src/main/java/com/chartboxapp/security/ChartBoxWebSecurityInitilazer.java
@@ -2,6 +2,6 @@ package com.chartboxapp.security;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class ChartBoxWebSecurityInitilazer extends AbstractSecurityWebApplicationInitializer{
+public class ChartBoxWebSecurityInitilazer /*extends AbstractSecurityWebApplicationInitializer*/{
 
 }
