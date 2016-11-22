d0c149e8a878bda5955bf99f53075ad02fafcb2b
satya123zx <satyakam.mohapatar91@gmail.com>
2016-11-23 00:08:59 +0530

    ==test example

--- ChartBoxApp/src/main/test/com/ChartBoxAppTest/ConfigurationTest.java
+++ ChartBoxApp/src/main/test/com/ChartBoxAppTest/ConfigurationTest.java
@@ -1,10 +1,29 @@
 package com.ChartBoxAppTest;
 
+import java.util.Arrays;
+
 public class ConfigurationTest {
 
 	public static void main(String[] args) {
 		
+		int[] arr = new int[] {6,5,4,2,3};
+		int remove = 5;
+		int[] tempArr = new int[arr.length-1];
 		
+	    for (int i = 0; i < arr.length; i++) {
+	    	if(arr[i]==remove){
+	    		 for (int j = i; j < arr.length-1; j++) {
+	    			arr[j]= arr[j+1];
+				}
+	    		for (int j = 0; j < arr.length-1; j++) {
+					tempArr[j]=arr[j];
+				}
+	    		break;
+	        }
+	    }
+	    arr = tempArr;
+	for (int i = 0;  i< arr.length; i++) {
+		System.out.print(", "+arr[i]);
 	}
-
+	}
 }
